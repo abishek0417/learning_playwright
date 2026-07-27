74146a77cc68ce069d23cf4d037a788dd636bc6a
abishek <ezioe6586@gmail.com>
2026-07-27 23:49:10 +0530

    testing statictable

--- day7/test_statictable.py
+++ day7/test_statictable.py
@@ -5,3 +5,4 @@ days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 @allure.title("verify the dropdown")
 def test_dropdown(page:Page):
     page.goto("https://testautomationpractice.blogspot.com/#")
+    page.locator("#male").check()
